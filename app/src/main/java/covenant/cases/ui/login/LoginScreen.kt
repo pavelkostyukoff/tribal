7db480046b4f42b19d93410e7fe2679cd8963b46
pavelkostyukoff <Soprano61@yandex.ru>
2021-09-25 13:15:44 +0300

    Login_screen

--- app/src/main/java/covenant/cases/ui/login/LoginScreen.kt
+++ app/src/main/java/covenant/cases/ui/login/LoginScreen.kt
@@ -1,27 +1,93 @@
 package covenant.cases.ui.login
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.ZeroCornerSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import covenant.tribal.R
 
 @Composable
 fun LoginScreen() {
-    LazyColumn(Modifier.fillMaxSize()) {
-        for (i in 0..1000) {
-            item { Text(text = "Hello word $i") }
-            item {
-                Row(Modifier.fillMaxWidth()) {
-                    Text(modifier = Modifier.weight(1f), text = "Hello ocean")
-                    Icon(imageVector = Icons.Default.Add, contentDescription = null)
+    ConstraintLayout(Modifier.fillMaxSize()) {
+        val (button, logo, editName, editPass) = createRefs()
+        Button(
+            onClick = { /* Do something */ },
+            // Assign reference "button" to the Button composable
+            // and constrain it to the top of the ConstraintLayout
+            modifier = Modifier
+                .constrainAs(button) {
+                    bottom.linkTo(parent.bottom, margin = 32.dp)
+                    linkTo(
+                        start = parent.start,
+                        end = parent.end,
+                        startMargin = 16.dp,
+                        endMargin = 16.dp
+                    )
                 }
-            }
+                .fillMaxWidth(0.8F)
+                .height(48.dp)
+        ) {
+            Text("Button")
         }
+        Card(
+            modifier = Modifier
+                .size(128.dp)
+                .constrainAs(logo) {
+                    top.linkTo(parent.top, 64.dp)
+                    linkTo(
+                        start = parent.start,
+                        end = parent.end,
+                        startMargin = 16.dp,
+                        endMargin = 16.dp
+                    )
+                },
+            shape = CircleShape,
+            elevation = 2.dp
+        ) {
+
+            Image(
+                painterResource(R.drawable.button_blue),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+        val login = remember { mutableStateOf(TextFieldValue("")) }
+
+        TextField(
+            value = login.value,
+            label = { Text(text = "Login")},
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .padding(16.dp)
+                .constrainAs(editName) {
+                    top.linkTo(logo.bottom, 64.dp)
+                }
+                .fillMaxWidth(),
+            onValueChange = {
+                login.value = it
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            )
+        )
     }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
 }
--- app/src/main/java/covenant/tribal/MainActivity.kt
+++ app/src/main/java/covenant/tribal/MainActivity.kt
@@ -7,7 +7,6 @@ import covenant.cases.ui.login.LoginScreen
 import covenant.cases.ui.theme.TribalChatsTheme
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
